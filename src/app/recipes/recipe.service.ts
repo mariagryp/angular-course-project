a6dd891311a60af0c5ace64005a7003a78cdb591
Maria G <maria.grypishyna@gmail.com>
2021-11-30 14:01:52 +0100

    first commit

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { Recipe } from './recipe.model';
+
+@Injectable()
+export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>();
+
+  /* private recipes: Recipe[] = [
+        new Recipe('A Test Recipe', 'This is simply a test. Try Another recipe', 
+        'https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80', 
+        [
+            new Ingredient('Meat', 1),
+            new Ingredient('French fries', 20)
+        ]),
+        new Recipe('AnotherTest Recipe', 'This is simply a yummy food! Enjoy it!', 
+        'https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+        [
+            new Ingredient('Buns', 2),
+            new Ingredient('Meat', 1),
+
+        ]), 
+      ];*/
+
+  private recipes: Recipe[] = [];
+
+  constructor(private slService: ShoppingListService) {}
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+  getRecipe(index: number) {
+    return this.recipes[index];
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  deleteRecipe(index: number) {
+    this.recipes.splice(index, 1);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+}
